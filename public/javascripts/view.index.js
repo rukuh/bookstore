8e55216cbba36b80485a0f868793d93726dbebf6
Luke Hyun <Luke Hyun>
2016-03-12 15:38:45 -0500

    Third commit

--- public/javascripts/view.index.js
+++ public/javascripts/view.index.js
@@ -26,22 +26,101 @@ $(function() {
   var fact = $('#new-fact').val();*/
   
   var bookshelf = $.getJSON("bookshelf.json", function() {
-	console.log( "success" );
-	console.log(bookshelf.responseJSON);
     $.each(bookshelf.responseJSON, function (key, data) {
-	  //console.log(data);
-	  console.log(data.title);
-      //$.each(data, function (index, data) {
-      $('tr').after().parent().append("<img src="+data.cover+" class=\"cover\">");
-      $('tr').after().parent().append("<p>Title: "+data.title+"</p>");
-	  $('tr').after().parent().append("<p>Author: "+data.author+"</p>");
-	  $('tr').after().parent().append("<p>Short Description: "+data.short_desc+"</p>");
-	  $('tr').after().parent().append("<p>Long Description: "+data.long_desc+"</p>");
-      //});
+      //$('.indicators').prepend('<li data-target="#bookshelf" data-slide-to='+key+'"</li>');
+      $('#bookshelf').after().parent().append('<div class="book"><img class="cover" src='+data.cover+'>');
+      $('.book:last').append('<div class="title">Title: '+data.title+'</div>');
+	    $('.book:last').append('<div class="author">Author: '+data.author+'</div>');
+	    $('.book:last').append('<div class="short_desc">Short Description: '+data.short_desc+'</div>');
+	    $('.book:last').append('<div class="long_desc" data="'+data.long_desc+'"</div></div>');
+      $('.book:last').append('<button class="add_to_cart">Add to Cart</button></div>');
     });
   });
-  
-  $('img').on('cpck', function() {
 
+  var modal = (function(){
+    var 
+    method = {},
+    $overlay,
+    $modal,
+    $content,
+    $close;
+
+    // Append the HTML
+    $overlay = $('<div id="overlay"></div>');
+    $modal = $('<div id="modal"></div>');
+    $content = $('<div id="content"></div>');
+    $close = $('<a id="close" href="#">close</a>');
+
+    $modal.hide();
+    $overlay.hide();
+    $modal.append($content, $close);
+
+    $(document).ready(function(){
+      $('body').append($overlay, $modal);
+    });
+
+    // Center the modal in the viewport
+    method.center = function () {
+      var top, left;
+
+     top = Math.max($(window).height() - $modal.outerHeight(), 0) / 2;
+     left = Math.max($(window).width() - $modal.outerWidth(), 0) / 2;
+
+     $modal.css({
+         top:top + $(window).scrollTop(), 
+         left:left + $(window).scrollLeft()
+     });
+    };
+
+    // Open the modal
+    method.open = function (settings) {
+      $content.empty().append(settings.content);
+
+      var cover = $('.active').find('.cover').attr('src');
+      var title = $('.active').find('.title').text();
+      var author = $('.active').find('.author').text();
+      var long_desc = $('.active').find('.long_desc').attr('data');
+
+      $('#content').append('<img class="large_cover" src='+cover+'>')
+      $('#content').append('<div class="title">'+title+'</div')
+      $('#content').append('<div class="author">'+author+'</div')
+      $('#content').append('<div class="long_desc">Long Description:'+long_desc+'</div')
+      $('#content').append('<button class="add_to_cart">Add to Cart</button></div>');
+
+      $modal.css({
+          width: settings.width || 'auto', 
+          height: settings.height || 'auto'
+      })
+
+      method.center();
+
+      $(window).bind('resize.modal', method.center);
+
+      $modal.slideDown();
+      $overlay.show();
+    };
+    
+    // Close the modal
+    method.close = function () {
+      $modal.hide();
+      $overlay.hide();
+      $content.empty();
+      $(window).unbind('resize.modal');
+    };
+
+    $close.click(function(e){
+      e.preventDefault();
+      method.close();
+    });
+
+    return method;
+  }());
+  
+  $(document).ready(function(){
+    $(document).on('click','img',function() {
+      $(this).parent().addClass('active');
+		  modal.open({ content: '' });
+      $(this).parent().removeClass('active');
+    });
   });
 });

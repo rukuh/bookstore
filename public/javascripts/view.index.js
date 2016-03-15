8846ca523d4c3ad2dbaa6d697763999d24bca0f6
Luke Hyun <Luke Hyun>
2016-03-14 22:14:48 -0400

    Fixed multiple ajax requests for bookshelf.json

--- public/javascripts/view.index.js
+++ public/javascripts/view.index.js
@@ -1,67 +1,46 @@
 $(function() {	
-  /*$('#add-new-fact').cpck(function() {
-    
-    var name = $('#right-column h2').text();
-    var fact = $('#new-fact').val();
-  
-    $.ajax({
-      type: "POST",
-      url: "/hero/add-fact",
-      data: JSON.stringify({ name: name, fact: fact }),
-      contentType: "apppcation/json; charset=utf-8",
-      dataType: "json",
-      success: function(data) {
-        $('<p>').appendTo('#facts').text(fact);
-        $('#new-fact').val('');
-      },
-      error: function(err) {
-        var msg = 'Status: ' + err.status + ': ' + err.responseText;
-        alert(msg);
-      }
-    });
-    return false;
-  });
-  
-  var name = $('#right-column h2').text();
-  var fact = $('#new-fact').val();*/
-
   var cart = [];
-  var current = 0;
 
-  function Shelve(current) {
-    console.log(current);
-    var bookshelf = $.ajax({
-      url: 'bookshelf.json',
-      dataType: 'json',
-      beforeSend: function() {
-        $('.loading').show();
-      },
-      complete: function() {
-        $.each(bookshelf.responseJSON.slice(current,current+60), function (key, data) {
-          if ( key < 60 ) {
-            var book = $('<div class="book"></div>');
-            book.css({ 'background' : 'url('+data.cover+')', 'background-size' : '100%' })
-            book.append('<div class="id" style="display:none">'+data._id+'</div>');
-            book.append('<div class="cover" src='+data.cover+'>');
-            book.append('<div class="title">'+data.title+'</div>');
-            book.append('<div class="author">by '+data.author+'</div>');
-            book.append('<div class="short_desc">'+data.short_desc+'</div>');
-            book.append('<div class="long_desc">'+data.long_desc+'</div>');
-            book.append('<div class="genre" style="display: none">'+data.genre+'</div>');
-            book.append('<button class="add_to_cart" style="display:none"><span>Add to Cart </span></button>');
+  // Render bookshelves in ajax callback due to asynchonous request
+  var bookshelf = $.ajax( 'bookshelf.json' )
+    .done( function(responseJSON) {
+      var current = 0;
 
-            $('#bookshelf').detach().append(book).appendTo($('.wrapper'));
-          } else {
-              return false;
-          }
-        });
-        $('.loading').hide();
+      Shelve(responseJSON,current)
+
+      $(window).scroll(function() {
+       if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+          current = current + 60;
+          Shelve(responseJSON,current);
+       }
+      });
+    });
+
+  // Append HTML for bookshelves
+  function Shelve(bookshelf,current) {
+    $('.loading').show();
+    $.each(bookshelf.slice(current,current+60), function (key, data) {
+      if ( key < 60 ) {
+        var book = $('<div class="book"></div>');
+        book.css({ 'background' : 'url('+data.cover+')', 'background-size' : '100%' })
+        book.append('<div class="id" style="display:none">'+data._id+'</div>');
+        book.append('<div class="cover" src='+data.cover+'>');
+        book.append('<div class="title">'+data.title+'</div>');
+        book.append('<div class="author">by '+data.author+'</div>');
+        book.append('<div class="short_desc">'+data.short_desc+'</div>');
+        book.append('<div class="long_desc">'+data.long_desc+'</div>');
+        book.append('<div class="genre" style="display: none">'+data.genre+'</div>');
+        book.append('<button class="add_to_cart" style="display:none"><span>Add to Cart </span></button>');
+
+        $('#bookshelf').detach().append(book).appendTo($('.wrapper'));
+      } else {
+          return bookshelf;
       }
     });
+    $('.loading').hide();
   }
 
-  Shelve(0);
-
+  // Dynamic modal takes contents as parameter
   var modal = (function(){
     var method = {}, $overlay, $modal, $content, $close;
 
@@ -129,6 +108,7 @@ $(function() {
     return method;
   }());
   
+  // Event listeners for styles and add to cart functionality
   $(document).ready(function(){
     $('#bookshelf').on('mouseenter','.book',function() {
       $(this).children('button').show();
@@ -175,13 +155,6 @@ $(function() {
       event.preventDefault();
       $('.book').hide();
       $('.title:contains('+$(this).val()+')').parent().show();
-    });
-
-    $(window).scroll(function() {
-       if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
-          current = current + 60;
-          Shelve(current);
-       }
     });
   });
 });

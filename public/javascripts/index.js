0d884bffe4a14266f6438c9c909ddd6133bb9e34
Luke Hyun <Luke Hyun>
2016-03-27 01:29:55 -0400

    Fixed issue with requesting pages when there were no more documents and increased readability of code

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,41 +1,42 @@
-$(function() {
+$( function() {
   var current = 0;
   var url = '/books';
-  // Render bookshelves in ajax callback due to asynchonous request
-  current = request(url,$('#filter').val(),$('#search').val(),current);
+
+  current = request( url, $( '#filter' ).val(), $( '#search' ).val(), current );
 
   // Load books on scroll
-  $(window).scroll(function() {
-    if( $(window).scrollTop() + $(window).height() > $(document).height() - 100 ) {
-      current = request(url,$('#filter').val(),$('#search').val(),current);
+  $( window ).scroll( function() {
+    if ( $( window ).scrollTop() + $( window ).height() > $( document ).height() - 100 ) {
+      // Conditional to check for end of book collection
+      if ( $( '.fin' ).length === 0 ) {
+        current = request( url, $( '#filter' ).val(), $( '#search' ).val(), current );
+      }
     }
-  });
+  } );
 
   // Load books on category select
-  $("#filter").change(function() {
-    $('#bookshelf').empty();
+  $( '#filter' ).change( function() {
+    $( '#bookshelf' ).empty();
     current = 0;
-    current = request(url,$('#filter').val(),$('#search').val(),current);
-  });
+    current = request( url, $( '#filter' ).val(), $( '#search' ).val(), current);
+  } );
 
   // Load books on search
-  $('#search').on('input propertychange paste', function(event) {
+  $( '#search' ).on( 'input propertychange paste', function( event ) {
     // Prevent multiple search requests while user is typing
-    delay(function(){
-      $('#bookshelf').empty();
+    delay( function() {
+      $( '#bookshelf' ).empty();
       current = 0;
-      current = request(url,$('#filter').val(),$('#search').val(),current);
-    }, 500);
-  });
+      current = request( url, $( '#filter' ).val(), $( '#search' ).val(), current);
+    }, 500 );
+  } );
+} );
 
-});
-
-function request(url,category,search,current) {
+function request( url, category, search, current ) {
   // Render bookshelves in ajax callback due to asynchonous request
-    $.ajax( url + '/' + current + '?' + 'category=' + category + '&' + 'search=' + search)
-    .done( function(responseJSON) {
-      // Initial load books
-      Shelve(responseJSON);
-    });
-    return current+30;
+  $.ajax( url + '/' + current + '?' + 'category=' + category + '&' + 'search=' + search )
+  .done( function( responseJSON ) {
+    Shelve( responseJSON );
+  } );
+  return current+30;
 }
--- routes/books.js
+++ routes/books.js
@@ -1,45 +1,46 @@
-var mongoose = require('mongoose');
-var express = require('express');
+var mongoose = require( 'mongoose' );
+var express = require( 'express' );
 var router = express.Router();
 
 // Mongoose connection to MongoDB
-mongoose.connect('mongodb://ec2-52-207-210-128.compute-1.amazonaws.com/test');
- 
-var db = mongoose.connection;
- 
-db.on('error', function (err) {
-  console.log('connection error', err);
-});
+mongoose.connect( 'mongodb://ec2-52-207-210-128.compute-1.amazonaws.com/test' );
 
-db.once('open', function () {
-  console.log('connected to db');
+var db = mongoose.connection;
+
+db.on( 'error', function( err ) {
+  console.log( 'connection error', err );
+} );
+
+db.once( 'open', function() {
+  console.log( 'connected to db' );
 
   // Mongoose schema definition
-  var bookSchema = mongoose.Schema({
+  var bookSchema = mongoose.Schema( {
     _id: String,
     cover: String,
     title: String,
     short_desc: String,
     long_desc: String,
     genre: String
-  });
+  } );
 
-  var Book = mongoose.model('Book', bookSchema,'bookshelf');
+  // Mongoose model definition
+  var Book = mongoose.model( 'Book', bookSchema, 'bookshelf' );
 
-  // GET bookshelf
-  router.get('/:skip', function(req, res) {
-  	var skip = parseInt(req.params.skip);
+  // GET bookshelf with parameters
+  router.get( '/:skip', function( req, res ) {
+  	var skip = parseInt( req.params.skip );
     var cat = req.query.category;
     var search = req.query.search;
 	  var query = Book.
-      find({'genre': new RegExp(cat),'title': new RegExp(search)}).
-      limit(30).
-      skip(skip);
+      find( { 'genre': new RegExp( cat ), 'title': new RegExp( search ) } ).
+      limit( 30 ).
+      skip( skip );
 
-    query.exec(function(e,docs){
-	  res.json(docs);
-    });
-  });
-});
+    query.exec( function( e, docs ) {
+	    res.json( docs );
+    } );
+  } );
+} );
 
 module.exports = router;
